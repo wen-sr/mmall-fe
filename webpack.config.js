d52cdf9fd7a820c6ac8945900c767f6407500045
wen-sr <793818714@qq.com>
2017-08-19 00:00:47 +0800

    initial

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
 * @Author: wen-sr
 * @Date:   2017-08-18 21:33:47
 * @Last Modified by:   wen-sr
-* @Last Modified time: 2017-08-18 23:49:36
+* @Last Modified time: 2017-08-19 00:00:16
 */
 //加载webpack对象，plugins里面使用
 var webpack 			= require("webpack");
@@ -36,12 +36,24 @@ module.exports = {
     externals : {
     	'jquery' : 'window.jQuery'
     },
+    resolve : {
+        alias : {
+            node_modules    : __dirname + '/node_modules',
+            util            : __dirname + '/src/util',
+            page            : __dirname + '/src/page',
+            service         : __dirname + '/src/service',
+            image           : __dirname + '/src/image'
+        }
+    },
     plugins : [
+    	// 独立通用模块到js/base.js
     	new webpack.optimize.CommonsChunkPlugin({
     		name : 'common',
     		filename : 'js/base.bundle.js'
     	}),
+    	// 把css单独打包到文件里
     	new ExtractTextPlugin("css/[name].bundle.css"),
+    	// html模板的处理
     	new HtmlWebpackPlugin(getHtmlConfig('index')),
     	new HtmlWebpackPlugin(getHtmlConfig('admin')),
     	new HtmlWebpackPlugin(getHtmlConfig('a')),
@@ -50,6 +62,10 @@ module.exports = {
     	loaders : [{
     		test : /\.css$/,
     		loader : ExtractTextPlugin.extract("style-loader","css-loader")
+    	},{ 
+    		test: /\.(gif|png|jpg|woff|svg|eot|ttf)\??.*$/, loader: 'url-loader?limit=100&name=resource/[name].[ext]' 
+    	},{ 
+    		test: /\.string$/, loader: 'html-loader'
     	}]
     }
 };
